03791b09471f905c779e16075ab711effa46d151
mcguirepr89 <mcguirepr89@gmail.com>
2022-06-30 10:59:39 -0400

    fixed the bad logic

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -140,7 +140,7 @@ EOF
 fi
 
 # Install new rclone.service if not already available
-if [ -f $HOME/BirdNET-Pi/templates/rclone.service ];then
+if ! [ -f $HOME/BirdNET-Pi/templates/rclone.service ];then
  cat << EOF > $HOME/BirdNET-Pi/templates/rclone.service
 [Unit]
 Description=Backup Tool
