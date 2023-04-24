dfea43ed5d690045bdd343a424a1a2514a04a493
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2023-04-24 11:33:31 -0400

    remove everything

--- scripts/birdnet_log.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-journalctl --no-hostname -q -o short -fu birdnet_analysis -ubirdnet_server -uextraction | sed "s/$(date "+%b %d ")//g;s/${HOME//\//\\/}\///g;/Line/d;/find/d;/systemd/d;s/ .*\[.*\]: /---/"
--- scripts/update_species.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Update the species list
-#set -x
-source /etc/birdnet/birdnet.conf
-if [ -f $HOME/BirdNET-Pi/scripts/birds.db ];then
-sqlite3 $HOME/BirdNET-Pi/scripts/birds.db "SELECT DISTINCT(Com_Name) FROM detections" | sort >  ${IDFILE}
-fi
