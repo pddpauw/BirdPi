52cca19150de7956d6078dd91a74e5bbaf430655
pddpauw <148589775+pddpauw@users.noreply.github.com>
2023-12-16 21:45:08 +0100

    Update changes_pi_5.sh
    new - closing apache and starting caddy

--- scripts/changes_pi_5.sh
+++ scripts/changes_pi_5.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+# Execute the necessary changes for difference in set up on the Pi5 (bookworm):
 
+# Turn off Apache2 
+sudo systemctl disable apache2 && sudo systemctl stop apache2
+echo "Apached2 disabled and stopped"
+
+# Enable and turn on Caddy
+sudo systemctl enable caddy && sudo systemctl start caddy
+echo "Caddy enabled and started"
