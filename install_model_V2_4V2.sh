a40d2b1327dc5c5f8119bde6423c55ce95d8e455
pddpauw <148589775+pddpauw@users.noreply.github.com>
2024-02-14 21:48:34 +0100

    Update install_model_V2_4V2.sh

--- install_model_V2_4V2.sh
+++ install_model_V2_4V2.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+# this script will replace the V2.4 model file with the new V2.4V2 model file
 
+set -x
+
+if [ "$EUID" == 0 ]
+  then echo "Please run as a non-root user."
+  exit
+fi
+
+# Simple new installer
+HOME=$HOME
+USER=$USER
+
+export HOME=$HOME
+export USER=$USER
+
+wget -P $HOME/BirdNET-Pi/model https://github.com/pddpauw/BirdPi/raw/5dc2a1edec6f669f8baff06be08b6cc996db36a2/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_V2_FP16%20(1).tflite
+
+mv $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16_OLD.tflite
+
+mv $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_V2_FP16%20(1).tflite $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite
+sudo chmod 664 $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite
+
+$HOME/BirdNET-Pi/scripts/restart_services.sh
