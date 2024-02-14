10802aee43d18b00f451befbbee389b234658e3b
pddpauw <148589775+pddpauw@users.noreply.github.com>
2024-02-14 22:10:47 +0100

    Update install_model_V2_4V2.sh

--- install_model_V2_4V2.sh
+++ install_model_V2_4V2.sh
@@ -15,11 +15,11 @@ USER=$USER
 export HOME=$HOME
 export USER=$USER
 
-wget -P $HOME/BirdNET-Pi/model https://github.com/pddpauw/BirdPi/raw/5dc2a1edec6f669f8baff06be08b6cc996db36a2/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_V2_FP16%20(1).tflite
+wget -P $HOME/BirdNET-Pi/model https://github.com/pddpauw/BirdPi/raw/5dc2a1edec6f669f8baff06be08b6cc996db36a2/model/NEW_BirdNET_GLOBAL_6K_V2.4_Model_FP16.tflite
 
 mv $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16_OLD.tflite
 
-mv $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_V2_FP16%20(1).tflite $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite
+mv $HOME/BirdNET-Pi/model/NEW_BirdNET_GLOBAL_6K_V2.4_Model_FP16.tflite $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite
 sudo chmod 664 $HOME/BirdNET-Pi/model/BirdNET_GLOBAL_6K_V2.4_MData_Model_FP16.tflite
 
 $HOME/BirdNET-Pi/scripts/restart_services.sh
