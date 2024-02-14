c09419538b3fd342642e84ef6907e950ed815fba
pddpauw <148589775+pddpauw@users.noreply.github.com>
2024-02-14 21:58:16 +0100

    Update replace_lower_bandwith.sh

--- replace_lower_bandwith.sh
+++ replace_lower_bandwith.sh
@@ -19,13 +19,13 @@ wget -P $HOME/BirdNET-Pi/scripts https://github.com/pddpauw/BirdPi/raw/222576a3c
 wget -P $HOME/BirdNET-Pi/scripts https://github.com/pddpauw/BirdPi/raw/7dcd54e6624c9f5394d940dd893ad46255d72b64/scripts/LB_overview.php
 
 mv $HOME/BirdNET-Pi/scripts/daily_plot.py $HOME/BirdNET-Pi/scripts/daily_plot_OLD.py 
-mv $HOME/BirdNET-Pi/scripts/daily_plot.py $HOME/BirdNET-Pi/scripts/daily_plot_OLD.py 
+mv $HOME/BirdNET-Pi/scripts/overview.php $HOME/BirdNET-Pi/scripts/overview_OLD.php 
 
 mv $HOME/BirdNET-Pi/scripts/LB_daily_plot.py $HOME/BirdNET-Pi/scripts/daily_plot.py 
 mv $HOME/BirdNET-Pi/scripts/LB_overview.php $HOME/BirdNET-Pi/scripts/overview.php
 
 sudo chmod +x $HOME/BirdNET-Pi/scripts/daily_plot.py
 sudo chmod 775 $HOME/BirdNET-Pi/scripts/daily_plot.py
-sudo chmod 664 $HOME/BirdNET-Pi/scripts/daily_plot.py
+sudo chmod 664 $HOME/BirdNET-Pi/scripts/overview.php
 
 $HOME/BirdNET-Pi/scripts/restart_services.sh
